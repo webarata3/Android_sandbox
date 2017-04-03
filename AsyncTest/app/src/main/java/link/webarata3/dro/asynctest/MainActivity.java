3730644447a51fcfaec995c56fc6e4f3d5c24fb9
webarata3 <webarata3@gmail.com>
2017-04-03 17:39:11 +0900

    ダイアログを使わないように変更

--- AsyncTest/app/src/main/java/link/webarata3/dro/asynctest/MainActivity.java
+++ AsyncTest/app/src/main/java/link/webarata3/dro/asynctest/MainActivity.java
@@ -1,82 +1,30 @@
 package link.webarata3.dro.asynctest;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.widget.TextView;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import link.webarata3.dro.asynctest.R;
-
-import static android.content.ContentValues.TAG;
 
 // http://qiita.com/amay077/items/b5f4e98b50d7fbcbaaec
-public class MainActivity extends AppCompatActivity implements TestThread.TestTask {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
-    private ExecutorService pool;
-
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pool = Executors.newSingleThreadExecutor();
-
-        textView = (TextView) findViewById(R.id.textView);
-        findViewById(R.id.begin_button).setOnClickListener(view -> {
-            textView.setText("処理中");
-            Log.d(TAG, "Start");
-            pool.submit(new TestThread(this));
-        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
-    }
-
-    @Override
-    public void progressUpdate(final int test) {
-        runOnUiThread(new Runnable() {
-            public void run() {
-                textView.setText(test + "%");
-            }
-        });
-    }
-}
-
-class TestThread extends Thread {
-    interface TestTask {
-        void progressUpdate(int test);
-    }
-
-    private TestTask testTask;
-
-    TestThread(TestTask testTask) {
-        this.testTask = testTask;
-    }
-
-    @Override
-    public void run() {
-        Log.d(TAG, "Run task A. ThreadId:" + Thread.currentThread().getId());
-        try {
-            for (int i = 0; i < 100; i++) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                testTask.progressUpdate(i);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
---
+++ AsyncTest/app/src/main/java/link/webarata3/dro/asynctest/MainFragment.java
@@ -0,0 +1,87 @@
+package link.webarata3.dro.asynctest;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainFragment extends Fragment implements TestThread.TestTask {
+    private AppCompatTextView textView;
+    private ExecutorService pool;
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View fragment = inflater.inflate(R.layout.fragment_main, container, false);
+        textView = (AppCompatTextView) fragment.findViewById(R.id.textView);
+
+        fragment.findViewById(R.id.beginButton).setOnClickListener(view -> {
+            textView.setText("処理開始");
+            pool = Executors.newSingleThreadExecutor();
+            pool.submit(new TestThread(this));
+        });
+
+        return fragment;
+    }
+
+    // http://qiita.com/syarihu/items/a8bb189d33585238c287
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void progressUpdate(int test) {
+        final int c = test;
+        getActivity().runOnUiThread(() -> {
+            textView.setText(c + "");
+        });
+    }
+
+}
+
+class TestThread extends Thread {
+    private static final String TAG = TestThread.class.getSimpleName();
+
+    interface TestTask {
+        void progressUpdate(int test);
+    }
+
+    private TestTask testTask;
+
+    TestThread(TestTask testTask) {
+        this.testTask = testTask;
+    }
+
+    @Override
+    public void run() {
+        Log.d(TAG, "Run task A. ThreadId:" + Thread.currentThread().getId());
+        try {
+            for (int i = 0; i <= 100; i++) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                testTask.progressUpdate(i);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
